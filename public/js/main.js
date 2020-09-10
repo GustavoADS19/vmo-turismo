6678441d715b0c8d88e41c5c5d523690b2136fc0
Math33w <matheus.ibrahim@hotmail.com>
2020-09-10 01:31:03 -0300

    Inserção do main.js

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+//VMO Turismo - main.js
+//Codificado por Matheus Ibrahim, 2020
+
+//Set de variáveis
+const fades = document.querySelectorAll(".section-fade");
+
+//Funções para rodar inicialmente
+scrollCheck();
+
+//Menu sumindo
+window.addEventListener("scroll", event => {
+    if (event.path[1].pageYOffset != 0 && document.querySelector("header")) {
+        document.querySelector("header").classList.add("offset");
+    } else {
+        document.querySelector("header").classList.remove("offset");
+    }
+    
+    scrollCheck();
+});
+
+//Detectar se objeto está no viewport
+function isInViewport(element) {
+    const bounding = element.getBoundingClientRect();
+    return (
+        bounding.top < 700
+    );
+}
+
+//Checagem ao scrollar a tela
+function scrollCheck(event) {
+    for (var fade of fades) {
+        if (isInViewport(fade)) {
+            fade.classList.remove("section-fade");
+        }
+    }
+}
