a73ea8c57616dc3e282af7a2ce752ecb3df50b97
Math33w <matheus.ibrahim@hotmail.com>
2020-09-10 23:15:40 -0300

    Menu que esconde e aparece

--- public/js/main.js
+++ public/js/main.js
@@ -8,18 +8,38 @@ const menuMobile = document.querySelector(".menu-mobile");
 const botaoHamburger = document.querySelector(".botao-hamburger");
 const menuClose = document.querySelector(".menu-close");
 
+var mouseY = 0;
+var pageYOffset = 0;
+
 //Funções para rodar inicialmente
 scrollCheck();
 
 //Menu sumindo
 window.addEventListener("scroll", event => {
-    if (event.path[1].pageYOffset != 0 && document.querySelector("header")) {
+    pageYOffset = event.path[1].scrollY;
+    console.log(event);
+
+    if (pageYOffset != 0 && document.querySelector("header") && window.innerWidth > 767 && mouseY > 200) {
         document.querySelector("header").classList.add("offset");
     } else {
         document.querySelector("header").classList.remove("offset");
     }
     
     scrollCheck();
+    mouseMoveCheck(event.path[1].pageYOffset);
+});
+
+window.addEventListener("resize", event => {
+    if (window.innerWidth < 767) {
+        document.querySelector("header").classList.remove("offset");
+    }
+    
+    scrollCheck();
+});
+
+window.addEventListener("mousemove", event => {
+    mouseY = event.screenY;
+    mouseMoveCheck(event.offsetY);
 });
 
 //Detectar se objeto está no viewport
@@ -46,3 +66,11 @@ menuClose.addEventListener("click", event => {
 botaoHamburger.addEventListener("click", event => {
     menuMobile.classList.remove("offset-2");
 });
+
+function mouseMoveCheck(offset) {
+    if (mouseY <= 200 ||  pageYOffset == 0) {
+        document.querySelector("header").classList.remove("offset");
+    } else if (mouseY > 200) {
+        document.querySelector("header").classList.add("offset");
+    }
+}
